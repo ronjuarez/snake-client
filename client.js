a4a2c9c604a4d0bc379bd8f9dd3a9d973b36e020
Ron Juarez <ronstewartjuarez@gmail.com>
2020-04-29 09:22:21 -0400

    added up movement and commented it out

--- client.js
+++ client.js
@@ -1,23 +1,36 @@
 const net = require('net');
+const {IP, PORT} = require('./constants');
+
 
 const connect = function() {
   const conn = net.createConnection({ 
-    host: 'localhost',
-    port: 50541
- });
-
- // interpret incoming data as text
-  conn.setEncoding('utf8'); 
-
-  
-  conn.on('connect', () => {
-    console.log("Successfully connected to the server");
-  })
-
-  conn.on('connect', () => {
-    conn.write('Name: RSJ');
+    host: IP,
+    port: PORT
+    
   });
+  conn.setEncoding('utf8');
+  conn.on('data', (data) => {
+    console.log('Server says: ', data);
+  
+}) 
+  conn.on('connect',() =>{
+    console.log(`Server Says: `,'successfully connected to the snake server' )
+})
+conn.on('connect', () => {
+    conn.write("Name: BOB")
+  
+})
+// conn.on('connect', (data) => {
+//     conn.write("Move: up")
+//     setTimeout(() =>  {
+//         conn.write("Move: up")
+//     },2000)
+// })
+return conn;
+}
 
-};
 
-module.exports = connect;
+
+
+
+module.exports = {connect};
