4576948d25b36a2881ba21b5f31a00d13b7d3de6
Ron Juarez <ronstewartjuarez@gmail.com>
2020-04-29 09:24:17 -0400

    add stdin

--- play.js
+++ play.js
@@ -2,7 +2,15 @@ const connect  = require('./client');
 /**
  * Establishes connection with the game server
  */
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  return stdin;
+}
 
+setupInput();
 
 console.log('Connecting ...');
 connect();
